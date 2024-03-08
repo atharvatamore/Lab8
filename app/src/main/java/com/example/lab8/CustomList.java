e641c8d86dd0586423fb76a589dd3e48c5aaa2ac
Atharva Tamore <atamore@gmail.com>
2024-03-07 22:57:04 -0700

    Count city will work

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -75,8 +75,7 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public int countCity(){
-        return 0;
-        //return cities.size();
+        return cities.size();
     }
 
 
